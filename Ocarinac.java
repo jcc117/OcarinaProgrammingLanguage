fa421330caa267901548afdaa798a57288909deb
Jordan Carr <jcc117@pitt.edu>
2018-06-29 22:52:07 -0400

    added small driver for testing

---
+++ Ocarinac.java
@@ -0,0 +1,50 @@
+// Ocarina compiler driver
+
+import java.util.*;
+
+public class Ocarinac{
+	public static void main(String args[]){
+		// No args: Print options
+		if(args.length == 0){
+			System.out.println("Ocarina compiler options");
+			System.out.println("-d\t\tDebug mode");
+			System.out.println("-v\t\tVersion Number");
+		}
+		// Debug mode
+		else if(args[0].equals("-d")){
+			// If nothing else, print debug menu options
+			if(args.length == 1)
+			{
+				System.out.println("Ocarina compiler debug options");
+				System.out.println("-t\t\tTokenizer Mode");
+			}
+			// Process the file in debug mode
+			// Will add more options later
+			else if(args.length == 3){
+				// Tokenizer mode
+				if(args[1].equals("-t")){
+
+				}
+				// Error
+				else{
+					System.out.println("Ocarina error: Invalid flag or filename");
+				}
+			}
+			// Error
+			else{
+				System.out.println("Ocarina error: Invalid flag or filename");
+			}
+		}
+		else if(args[0].equals("-v")){
+			System.out.println("Ocarina compiler version 0.1");
+		}
+		// Regular compiler mode (will change later)
+		else if(args.length == 2){
+
+		}
+		// Error
+		else{
+			System.out.println("Ocarina error: Invalid flag or file name");
+		}
+	}
+}
