022a618b479648fca5fe82aa5be1560758ac1542
Jordan Carr <jcc117@pitt.edu>
2018-07-28 00:35:24 -0400

    more cleanup

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#Build the interpreter
-
-jflex OcarinaLexer.flex
-java -jar java-cup-11a.jar OcarinaParser.cup
-javac ast/*.java
-javac symboltable/*.java
-javac -cp ".;java-cup-11a.jar" Ocarinac.java
