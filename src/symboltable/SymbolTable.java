3382efbed51705e468a298c1c9c7386cf5a024b6
Jordan Carr <jcc117@pitt.edu>
2018-07-29 12:38:04 -0400

    time to build a symboltable generator. woot

--- src/symboltable/SymbolTable.java
+++ src/symboltable/SymbolTable.java
@@ -2,7 +2,25 @@ package symboltable;
 import ast.*;
 
 public class SymbolTable{
-	public SymbolTable(){
+	private SageSym root;
+	
+	public SymbolTable(SageSym root){
+		this.root = root;
+	}
+
+	//Search for a symbol
+	public void search(String name){
 
 	}
+
+	//Go up a level in scope
+	public void rise(){
+
+	}
+
+	//Go down a level in scope
+	public void descend(){
+
+	}
+
 }
