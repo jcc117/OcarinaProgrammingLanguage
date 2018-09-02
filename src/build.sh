89c2f5d42e62d2cddbc8c88a06072fa749259f2b
Jordan Carr <jcc117@pitt.edu>
2018-09-02 19:07:32 -0400

    fixed up shell script files

--- src/build.sh
+++ src/build.sh
@@ -4,10 +4,6 @@
 cd lexer_parser
 jflex OcarinaLexer.flex
 java -jar ../java-cup-11a.jar OcarinaParser.cup
-javac -cp ".;java-cup-11.jar" *.java
 cd ..
-javac ast/*.java
-javac symboltable/*.java
-javac filecatalog/*.java
 javac -cp ".;java-cup-11a.jar" Ocarinac.java
 $SHELL
---
+++ src/clean.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#Clean up for another build
+
+rm ast/*.class
+rm symboltable/*.class
+rm filecatalog/*.class
+rm lexer_parser/*.class
+rm *.class
