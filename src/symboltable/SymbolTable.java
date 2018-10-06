053865d1fad0a6f9001a3c13bcf2ee51aa977783
Jordan Carr <jcc117@pitt.edu>
2018-10-05 22:36:19 -0400

    VERY rough logic in place for finding symbols within the table

--- src/symboltable/SymbolTable.java
+++ src/symboltable/SymbolTable.java
@@ -19,17 +19,202 @@ public class SymbolTable{
 	//one is violated with the given path.
 	public Sym getSymbol(String[] path, boolean isTypeSearch){
 		if(path.length == 0){
-			return null
+			return null;
 		}
 		else{
+			boolean found = false;
+			Sym thisScope = currentScope;
 			//Indicates the item under question can only be searched under a class table
 			if(isTypeSearch){
 				//Search current scope for first item in path under class scopes - float up til found
+				while(!found){
+					if(thisScope == null){
+						return null;
+					}
+					else if(thisScope instanceof MethodSym){
+						//Float up in scope
+						thisScope = thisScope.parent;
+					}
+					else if(thisScope instanceof ClassSym){
+						ClassSym result = ((ClassSym)thisScope).getClass(path[0]);
+						if(result == null){
+							thisScope = thisScope.parent;
+						}
+						else{
+							found = true;
+							thisScope = result;
+						}
+					}
+					else if(thisScope instanceof SageSym){
+						ClassSym result = ((SageSym)thisScope).getClass(path[0]);
+						if(result == null){
+							return null;	//Return null for now - will fill in looking in other tables later
+						}
+						else{
+							found = true;
+							thisScope = result;
+						}
+					}
+					else{
+						return null;
+					}
+				}
+
+				//Search the rest of the path
+				int index = 1;
+				while(index < path.length){
+					if(thisScope == null){
+						return null;
+					}
+					else if(thisScope instanceof ClassSym){
+						ClassSym result = ((ClassSym)thisScope).getClass(path[index]);
+						//Check the permissions of the result
+						if(result == null){
+							return null;
+						}
+						else if(result.protection == Sym.ProtectionLevel.PRIVATE){
+							return null;
+						}
+						//Will insert checks for PROTECTED here later
+						else{
+							thisScope = result;
+						}
+					}
+					else{
+						return null;
+					}
+					index++;
+				}
+				return thisScope;
 				//If found - search the rest of the path under class scopes only
 			}
 			else{
 				//If the symbol is the last one in the class search in the var and method tables
 				//Otherwise search the class tables
+				MethodScope thisMethodScope = currentMethodScope;
+				//If there is only 1 item in the path length, then this item can be a class, method, or variable
+				if(path.length == 1){
+					while(!found)
+					{
+						if(thisScope == null){
+							return null;
+						}
+						else if(thisScope instanceof SageSym){
+							//Search class table
+							//Search method table
+							//Search var table
+						}
+						else if(thisScope instanceof ClassSym){
+							//Search class table
+							//Search method table
+							//Search var table
+						}
+						else if(thisScope instanceof MethodSym){
+							//Look in the current method scope
+							if(thisMethodScope == null){
+								//Search var table
+							}
+							//Look in the current inner method scope
+							else{
+
+							}
+						}
+						else{
+							return null;
+						}
+					}
+				}
+				//If the path length is greater than 1, then the first item must be a class and all items seen until
+				//the last item is reached must be classes
+				else{
+					//Search current scope for first item in path under class scopes - float up til found
+					while(!found){
+						if(thisScope == null){
+							return null;
+						}
+						else if(thisScope instanceof MethodSym){
+							//Float up in scope
+							thisScope = thisScope.parent;
+						}
+						else if(thisScope instanceof ClassSym){
+							ClassSym result = ((ClassSym)thisScope).getClass(path[0]);
+							if(result == null){
+								thisScope = thisScope.parent;
+							}
+							else{
+								found = true;
+								thisScope = result;
+							}
+						}
+						else if(thisScope instanceof SageSym){
+							ClassSym result = ((SageSym)thisScope).getClass(path[0]);
+							if(result == null){
+								return null;	//Return null for now - will fill in looking in other tables later
+							}
+							else{
+								found = true;
+								thisScope = result;
+							}
+						}
+						else{
+							return null;
+						}
+					}
+					//Search the rest of the path until the second to last item
+					int index = 1;
+					while(index < (path.length - 1)){
+						if(thisScope == null){
+							return null;
+						}
+						else if(thisScope instanceof ClassSym){
+							ClassSym result = ((ClassSym)thisScope).getClass(path[index]);
+							//Check the permissions of the result
+							if(result == null){
+								return null;
+							}
+							else if(result.protection == Sym.ProtectionLevel.PRIVATE){
+								return null;
+							}
+							//Will insert checks for PROTECTED here later
+							else{
+								thisScope = result;
+							}
+						}
+						else{
+							return null;
+						}
+						index++;
+					}
+
+					//Once the end of the path has been reached search for the last item under all
+					//possible tables
+					if(thisScope == null){
+						return null;
+					}
+					else if(thisScope instanceof SageSym){
+						//Search class table
+						//Search method table
+						//Search var table
+					}
+					else if(thisScope instanceof ClassSym){
+						//Search class table
+						//Search method table
+						//Search var table
+					}
+					else if(thisScope instanceof MethodSym){
+						//Look in the current method scope
+						if(thisMethodScope == null){
+							//Search var table
+						}
+						//Look in the current inner method scope
+						else{
+
+						}
+					}
+					else{
+						return null;
+					}
+				}
 			}
 		}
 
